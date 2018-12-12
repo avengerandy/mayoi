f72139732792d14f51ada5e76b95b0715e861b4d
MazeR <mazer0701@gmail.com>
2018-12-12 23:05:05 +0800

    delete old code file

--- src/util/context.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pass: 0, //現在通過幾個測試
-    sourceName: "", //現在測試檔案名
-    allTsetCount: 0, //所有測試函數數量
-    nowCount: 0, //現在是第幾個測試函數
-    count: 1, //現在是第幾個測試檔案
-    subCount: 1 //現在是測試檔案中的第幾個測試函數
-}
