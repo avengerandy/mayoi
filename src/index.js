74c7e673d39ef638ec246464dfac8506efee3e3c
MazeR <mazer0701@gmail.com>
2020-11-28 20:59:59 +0800

    replace async library with eachSeries.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const async = require('async');
 const path = require('path');
 const Mock = require('../src/Mock.js');
 const patchConfig = require('./patchConfig.js');
+const eachSeries = require('./eachSeries.js');
 const getAllTestFiles = require('./getAllTestFiles.js');
 const runIfFunction = require('./runIfFunction.js');
 const printError = require('../src/printError.js');
 
 module.exports = {
-    run: function (config) {
+    run: async function (config) {
         config = patchConfig(config);
         const testFiles = getAllTestFiles(config);
         const count = {
@@ -17,50 +17,40 @@ module.exports = {
             passTestCount: 0
         };
 
-        function runTestFile (testFile) {
-            return new Promise(function (resolve, reject) {
-                async.eachSeries(testFile.tests, async function (test) {
-                    count.subTestCount++;
-                    try {
-                        await runIfFunction(testFile.startEach);
-                        await runIfFunction(test);
-                        count.passTestCount++;
-                        if (config.printPass) {
-                            console.log(`╠ ${count.subTestCount}.${test.name}\t=> pass`);
-                        }
-                        await runIfFunction(testFile.endEach);
-                    } catch (error) {
-                        console.log(`╠ ${count.subTestCount}.${test.name}\t=> fail`);
-                        printError(error);
+        async function runTestFile (testFile) {
+            await eachSeries(testFile.tests, async function (test) {
+                count.subTestCount++;
+                try {
+                    await runIfFunction(testFile.startEach);
+                    await runIfFunction(test);
+                    count.passTestCount++;
+                    if (config.printPass) {
+                        console.log(`╠ ${count.subTestCount}.${test.name}\t=> pass`);
                     }
-                }, async function (error) {
-                    if (error) reject(error);
-                    await runIfFunction(testFile.endFunction);
-                    count.allSubTestCount += count.subTestCount;
-                    resolve();
-                });
+                    await runIfFunction(testFile.endEach);
+                } catch (error) {
+                    console.log(`╠ ${count.subTestCount}.${test.name}\t=> fail`);
+                    printError(error);
+                }
+            });
+            await runIfFunction(testFile.endFunction);
+            count.allSubTestCount += count.subTestCount;
+        }
+
+        async function runTest () {
+            await eachSeries(testFiles, async function (testFilePath) {
+                count.fileTestCount++;
+                const testFile = require(testFilePath);
+                console.log('----------------------------------------');
+                console.log(count.fileTestCount + '. test ' + path.basename(testFilePath));
+                console.log('----------------------------------------');
+                await runIfFunction(testFile.startFunction);
+                count.subTestCount = 0;
+                await runTestFile(testFile);
             });
         }
 
-        function runTest () {
-            return new Promise(function (resolve, reject) {
-                async.eachSeries(testFiles, async function (testFilePath) {
-                    count.fileTestCount++;
-                    const testFile = require(testFilePath);
-                    console.log('----------------------------------------');
-                    console.log(count.fileTestCount + '. test ' + path.basename(testFilePath));
-                    console.log('----------------------------------------');
-                    await runIfFunction(testFile.startFunction);
-                    count.subTestCount = 0;
-                    await runTestFile(testFile);
-                }, function (error) {
-                    if (error) reject(error);
-                    resolve();
-                });
-            });
-        }
-
-        async.eachSeries([
+        await eachSeries([
             async () => await runIfFunction(config.startFunction),
             async () => await runTest(),
             async function () {
